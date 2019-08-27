197e6566cad1bb06376c379ddc79e9bb33ea4d89
eliassjogreen <eliassjogreen1@gmail.com>
2019-08-27 15:54:11 +0200

    Added mod.ts and changed all references to dejs.ts

--- dejs_test.ts
+++ dejs_test.ts
@@ -1,7 +1,7 @@
 const { Buffer, copy, cwd } = Deno;
 import { test, runTests } from 'https://deno.land/std/testing/mod.ts';
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
-import * as dejs from './dejs.ts';
+import * as dejs from './mod.ts';
 import escape from 'https://deno.land/x/lodash/escape.js';
 
 // renderTest
