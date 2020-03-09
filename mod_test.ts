6bba08cf8e73c3a30cead8f7b9becff5b5930afe
syumai <syumai@gmail.com>
2020-03-10 00:35:59 +0900

    Fixed prettier config

--- mod_test.ts
+++ mod_test.ts
@@ -1,7 +1,7 @@
 const { Buffer, copy, cwd } = Deno;
-import { assertEquals } from "./vendor/https/deno.land/std/testing/asserts.ts";
-import * as dejs from "./mod.ts";
-import escape from "./vendor/https/deno.land/x/lodash/escape.js";
+import { assertEquals } from './vendor/https/deno.land/std/testing/asserts.ts';
+import * as dejs from './mod.ts';
+import escape from './vendor/https/deno.land/x/lodash/escape.js';
 
 // renderTest
 (() => {
@@ -14,75 +14,75 @@ import escape from "./vendor/https/deno.land/x/lodash/escape.js";
 
   const testCases: Array<testCase> = [
     {
-      name: "Normal",
-      body: "normal test",
-      expected: "normal test"
+      name: 'Normal',
+      body: 'normal test',
+      expected: 'normal test',
     },
     {
-      name: "Escaped",
-      body: "<%= param %>",
-      param: "<div>test</div>",
-      expected: escape("<div>test</div>")
+      name: 'Escaped',
+      body: '<%= param %>',
+      param: '<div>test</div>',
+      expected: escape('<div>test</div>'),
     },
     {
-      name: "Raw",
-      body: "<%- param %>",
-      param: "<div>test</div>",
-      expected: "<div>test</div>"
+      name: 'Raw',
+      body: '<%- param %>',
+      param: '<div>test</div>',
+      expected: '<div>test</div>',
     },
     {
-      name: "Comment",
-      body: "<%# param %>",
-      param: "<div>test</div>",
-      expected: ""
+      name: 'Comment',
+      body: '<%# param %>',
+      param: '<div>test</div>',
+      expected: '',
     },
     {
-      name: "Evaluate if true",
-      body: "<% if (param) { %>test<% } %>",
+      name: 'Evaluate if true',
+      body: '<% if (param) { %>test<% } %>',
       param: true,
-      expected: "test"
+      expected: 'test',
     },
     {
-      name: "Evaluate if false",
-      body: "<% if (param) { %>test<% } %>",
+      name: 'Evaluate if false',
+      body: '<% if (param) { %>test<% } %>',
       param: false,
-      expected: ""
+      expected: '',
     },
     {
-      name: "Evaluate for",
-      body: "<% for (let i = 0; i < 3; i++) { %>Test<% } %>",
-      expected: "TestTestTest"
+      name: 'Evaluate for',
+      body: '<% for (let i = 0; i < 3; i++) { %>Test<% } %>',
+      expected: 'TestTestTest',
     },
     {
-      name: "Evaluate nested for",
+      name: 'Evaluate nested for',
       body:
-        "<% for (let i = 0; i < 2; i++) { %><% for (let j = 0; j < 2; j++) { %>Test<% } %><% } %>",
-      expected: "TestTestTestTest"
+        '<% for (let i = 0; i < 2; i++) { %><% for (let j = 0; j < 2; j++) { %>Test<% } %><% } %>',
+      expected: 'TestTestTestTest',
     },
     {
-      name: "Evaluate if true",
-      body: "<% if (param) { %>test<% } %>",
+      name: 'Evaluate if true',
+      body: '<% if (param) { %>test<% } %>',
       param: true,
-      expected: "test"
+      expected: 'test',
     },
     {
-      name: "Escaped without spacing",
-      body: "<%=param%>",
-      param: "<div>test</div>",
-      expected: escape("<div>test</div>")
+      name: 'Escaped without spacing',
+      body: '<%=param%>',
+      param: '<div>test</div>',
+      expected: escape('<div>test</div>'),
     },
     {
-      name: "Raw without spacing",
-      body: "<%-param%>",
-      param: "<div>test</div>",
-      expected: "<div>test</div>"
+      name: 'Raw without spacing',
+      body: '<%-param%>',
+      param: '<div>test</div>',
+      expected: '<div>test</div>',
     },
     {
-      name: "Comment without spacing",
-      body: "<%#param%>",
-      param: "<div>test</div>",
-      expected: ""
-    }
+      name: 'Comment without spacing',
+      body: '<%#param%>',
+      param: '<div>test</div>',
+      expected: '',
+    },
   ];
 
   for (const tc of testCases) {
@@ -93,7 +93,7 @@ import escape from "./vendor/https/deno.land/x/lodash/escape.js";
         await copy(buf, await dejs.render(tc.body, { param: tc.param }));
         const actual = buf.toString();
         assertEquals(actual, tc.expected);
-      }
+      },
     });
   }
 })();
@@ -109,22 +109,22 @@ import escape from "./vendor/https/deno.land/x/lodash/escape.js";
 
   const testCases: Array<testCase> = [
     {
-      name: "Normal",
-      fileName: "normal",
-      expected: "normal test"
+      name: 'Normal',
+      fileName: 'normal',
+      expected: 'normal test',
     },
     {
-      name: "Raw",
-      fileName: "raw",
-      param: "<div>test</div>",
-      expected: "<div>test</div>"
+      name: 'Raw',
+      fileName: 'raw',
+      param: '<div>test</div>',
+      expected: '<div>test</div>',
     },
     {
-      name: "Include",
-      fileName: "include",
-      param: "<div>test</div>",
-      expected: "<div>test</div>"
-    }
+      name: 'Include',
+      fileName: 'include',
+      param: '<div>test</div>',
+      expected: '<div>test</div>',
+    },
   ];
 
   for (const tc of testCases) {
@@ -135,12 +135,12 @@ import escape from "./vendor/https/deno.land/x/lodash/escape.js";
         await copy(
           buf,
           await dejs.renderFile(`${cwd()}/testdata/${tc.fileName}.ejs`, {
-            param: tc.param
+            param: tc.param,
           })
         );
         const actual = buf.toString();
         assertEquals(actual, tc.expected);
-      }
+      },
     });
   }
 })();
